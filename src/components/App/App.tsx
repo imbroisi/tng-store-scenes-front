046254d436bd9ef25aeda53983bd9e35f8be795a
Luiz Imbroisi <luizimbroisi@gmail.com>
2024-09-18 15:34:27 -0300

    any

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,50 @@ import CharactersContainer from '../CharactersContainer';
 import { Container } from './App.styles';
 import CharactersDataContainer from '../CharactersDataContainer';
 import LocaisContainer from '../LocaisContainer';
-import Box from '../Box';
+// import Box from '../Box';
 
 export default function App() {
+  const [episode, setEpisode] = useState('');
+  const [time, setTime] = useState('');
   const [localSelected, setLocalSelected] = useState('Bridge');
-  const [checkedCharacters, setCheckedCharacters] = useState({});
+  const [checkedCharacters, setCheckedCharacters] = useState<{ [key: string]: boolean }>({});
   const [optionsSelecteds, setOptionsSelecteds] = useState<{ [key: string]: any }>({});
+  
+  const handleSave = async () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (!confirm('Save scene?')){
+      console.log('==> not saved');
+      return;
+    }
+    const scene = {
+      episode_time: `${episode} ${time} OK`,
+      local: localSelected,
+      characters: Object.keys(checkedCharacters).map(character => {
+        return {
+          name: character,
+          actions: Object.keys(optionsSelecteds[character]).map(optionName => {
+            return {
+              name: optionName,
+              values: Object.keys(optionsSelecteds[character][optionName]).filter(key => optionsSelecteds[character][optionName][key])
+            }
+          })
+        }
+      })
+    };
+
+    console.log('==> scene', scene);
+
+
+    const response = await fetch('http://localhost:3001/save-scene', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(scene)
+    });
+
+    console.log('==> response', response);
+  }
 
   const handleLocalChanged = (local: string) => {
     setLocalSelected(local);
@@ -17,14 +55,14 @@ export default function App() {
   const handleCharacterChanged = (character: string, isChecked: boolean) => {
     setCheckedCharacters({
       ...checkedCharacters,
-      [character]: isChecked 
+      [character]: isChecked
     });
   }
 
   const handleOptionChanged = (
-    character: string, 
-    optionName: string, 
-    optionChanged: string, 
+    character: string,
+    optionName: string,
+    optionChanged: string,
     isChecked: boolean
   ) => {
     setOptionsSelecteds({
@@ -33,18 +71,58 @@ export default function App() {
         ...optionsSelecteds[character],
         [optionName]: {
           ...optionsSelecteds[character]?.[optionName],
-         [optionChanged]: isChecked
-       }
+          [optionChanged]: isChecked
+        }
       }
     });
   };
 
+  const handleEpisodeChange = (e: any) => {
+    setEpisode(e.target.value);
+  }
+
+  const handleTimeChange = (e: any) => {
+    console.log('==> time', e.target.value);
+    setTime(e.target.value);
+  }
+
+  const handleTmpPost = async () => {
+    const response = await fetch(`http://localhost:3001/search-scenes?local=Bridge&char=Data&action=TALKING&values=normal`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log('==> fetched scenes', data);
+  }
+
   return (
     <Container>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <div style={{ border: '1px solid #aaa', padding: '16px', margin:'16px' }}>
-          <label style={{ paddingTop: '12px' }}>Scene File Name:</label>
-          <input type="text" style={{ width: '97%' }} />
+        <div style={{ border: '1px solid #aaa', padding: '16px', margin: '16px' }}>
+          <div>
+            <label>
+              Episode:
+            </label>
+            <input type="text" value={episode} style={{ width: '97%' }} onChange={handleEpisodeChange} />
+          </div>
+          <div style={{ marginTop: '8px' }}>
+            <label>
+              Time:
+            </label>
+            <input type="text" value={time} style={{ width: '97%' }} onChange={handleTimeChange} />
+          </div>
+          <div>
+            <button style={{ marginTop: '16px' }} onClick={handleSave}>
+              Save
+            </button>
+
+            <button style={{ marginTop: '16px' }} onClick={handleTmpPost}>
+              TMP Get
+            </button>
+          </div>
         </div>
         <div style={{ display: 'flex' }}>
           <LocaisContainer localSelected={localSelected} onChange={handleLocalChanged} />
@@ -55,3 +133,47 @@ export default function App() {
     </Container>
   );
 }
+
+/*
+
+I have example they are in json, but must be saved in SQLite to be searchable. 
+
+
+{e to save the following informations in SQLite.
+In th
+  episode_time: 's04e12 00:01:00',
+  local: 'Corridor',
+  characters: [
+    {
+      name: 'Picard',
+      actions: [
+      {
+        name: 'TALKING',
+        values: ['normal', 'quietly', 'loudly']
+      },
+      {
+        name: 'FIGHTING',
+        values: ['fists', 'phasers', 'photonTorpedos']
+      }
+    },
+    {
+      name: 'Data',
+      actions: [
+      {
+        name: 'TALKING',
+        values: ['normal', 'quietly']
+      },
+      {
+        name: 'WALKING',
+        values: ['normal', 'fast', 'slow']
+      }
+    },
+  ]
+}
+
+So to bem possible to search for a scene in the "Corridor", with character "Picard", action "TALKING" with values ["loudly", "normal"].
+
+the return must be the episode_time.
+
+
+*/
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
-  worker.start();
-}
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
